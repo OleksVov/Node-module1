b138c27efce247745e28f10017e6c0c18cc4fc7e
Oleksandr <alex.vovchok@gmail.com>
2023-06-14 22:57:38 +0300

    added fs and path

--- contacts.js
+++ contacts.js
@@ -1,26 +1,32 @@
-// Зроби імпорт модулів fs і path для роботи з файловою системою
-// Створи змінну contactsPath і запиши в неї шлях до файлу contacts.json. Для складання шляху використовуй методи модуля path.
-// Додай функції для роботи з колекцією контактів. У функціях використовуй модуль fs та його методи readFile() і writeFile()
-// Зроби експорт створених функцій через module.exports
 
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
 
-// TODO: задокументувати кожну функцію
+const fs = require('fs').promises;
+
+const path = require('node:path');
+
+ const contactsPath = path.join(__dirname, "contacts.json");
+
+ console.log(contactsPath);
+
+
+
 function listContacts() {
-    // ...твій код
+  
   }
   
   function getContactById(contactId) {
-    // ...твій код
+  
   }
   
   function removeContact(contactId) {
-    // ...твій код
+  
   }
   
   function addContact(name, email, phone) {
-    // ...твій код
+  
+  };
+
+
+  module.exports = {
+    listContacts,
   }
