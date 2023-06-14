1052ffa22eac1bdea019630dfc8399986b7c04ef
Oleksandr <alex.vovchok@gmail.com>
2023-06-14 20:57:44 +0300

    Update contacts.js

---
+++ contacts.js
@@ -0,0 +1,26 @@
+// Зроби імпорт модулів fs і path для роботи з файловою системою
+// Створи змінну contactsPath і запиши в неї шлях до файлу contacts.json. Для складання шляху використовуй методи модуля path.
+// Додай функції для роботи з колекцією контактів. У функціях використовуй модуль fs та його методи readFile() і writeFile()
+// Зроби експорт створених функцій через module.exports
+
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+    // ...твій код
+  }
+  
+  function getContactById(contactId) {
+    // ...твій код
+  }
+  
+  function removeContact(contactId) {
+    // ...твій код
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твій код
+  }
