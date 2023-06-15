946a90953d3693eb9d79290239ff8850a61b32ca
Oleksandr <alex.vovchok@gmail.com>
2023-06-15 23:15:41 +0300

    finish step 4

--- contacts.js
+++ contacts.js
@@ -1,32 +1,50 @@
-
-
 const fs = require('fs').promises;
 
 const path = require('node:path');
 
- const contactsPath = path.join(__dirname, "contacts.json");
+const {nanoid} = require('nanoid');
 
- console.log(contactsPath);
+ const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-
-
-function listContacts() {
-  
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data)
   }
   
-  function getContactById(contactId) {
-  
+  async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
   }
   
-  function removeContact(contactId) {
-  
+  async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+  if(index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
   }
   
-  function addContact(name, email, phone) {
-  
+  async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone
+  }
+  contacts.push(newContact)
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
   };
 
 
   module.exports = {
     listContacts,
+    getContactById,
+    removeContact,
+    addContact,
   }
